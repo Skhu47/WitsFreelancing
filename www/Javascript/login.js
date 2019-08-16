745e9307955b3c658f245bc7d12de29bb994d985
PermanentPortionX <1627982@students.wits.ac.za>
2019-08-16 15:19:03 +0200

    added php plugin

--- www/Javascript/login.js
+++ www/Javascript/login.js
@@ -1,34 +1,62 @@
-
 $(document).ready(function(){
-    $('#login_btn').click(function(){
-        let stud_id = $('#Username').val(); //student_id
-        console.log(stud_id);
-        let stud_pass = $('#Password').val(); //student password
-        console.log(stud_pass);
-        let res = $.ajax({
-            type: 'POST',
-            url: 'http://1627982.ms.wits.ac.za/~student/auth.php',
-            data:{USERNAME: stud_id,PASSWORD :stud_pass},
-            dataType: "text",
-            /*success: function (res) {
+    $("#login_btn").on("click", (e) =>{
 
-                alert(res);}*/
-        });
-        res.done(function (msg) {
-            console.log(msg);
-        });
+        e.preventDefault();
 
-        res.fail(function (jqXHR, status) {
-            console.log(status);
-        });
-        //$("#div1").load("html/mainpage.html");
-        //window.location.href="www/html/mainpage.html"; // upon validation we need to go to the home page
-        /*$.post(
-            "1627982.ms.wits.ac.za/~student/auth.php",
-            {USERNAME: stud_id,PASSWORD :stud_pass},function (data) {
-                alert(data);
+        const options = {
+            method: "post",
+            timeout: 10000,
+            data: {
+                USERNAME: $("#Username").val(),
+                PASSWORD: $("#Password").val()
             }
-        );*/
-        console.log("after the cond");
+        };
+        const url = "http://1627982.ms.wits.ac.za/~student/auth.php";
+
+        cordova.plugin.http.sendRequest(url, options,
+            function (response) {
+                //success
+                let results = response.data; //data from server, it's a string, must be converted to an appropriate format
+                //e.g. json
+                alert("Mojo is awesome, here are the results from server " + results);
+            },
+            function (response) {
+                //fail
+            },
+            function (response) {
+                //permission denied
+            })
     });
+
+    // $('#login_btn').click(function(){
+    //     let stud_id = $('#Username').val(); //student_id
+    //     console.log(stud_id);
+    //     let stud_pass = $('#Password').val(); //student password
+    //     console.log(stud_pass);
+    //     let res = $.ajax({
+    //         type: 'POST',
+    //         url: 'http://1627982.ms.wits.ac.za/~student/auth.php',
+    //         data:{USERNAME: stud_id,PASSWORD :stud_pass},
+    //         dataType: "text",
+    //         /*success: function (res) {
+    //
+    //             alert(res);}*/
+    //     });
+    //     res.done(function (msg) {
+    //         console.log(msg);
+    //     });
+    //
+    //     res.fail(function (jqXHR, status) {
+    //         console.log(status);
+    //     });
+    //     //$("#div1").load("html/mainpage.html");
+    //     //window.location.href="www/html/mainpage.html"; // upon validation we need to go to the home page
+    //     /*$.post(
+    //         "1627982.ms.wits.ac.za/~student/auth.php",
+    //         {USERNAME: stud_id,PASSWORD :stud_pass},function (data) {
+    //             alert(data);
+    //         }
+    //     );*/
+    //     console.log("after the cond");
+    // });
 });
