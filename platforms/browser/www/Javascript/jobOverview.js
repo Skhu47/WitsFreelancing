e4cb10aaab015bb169825e5cc272d93f27a31d1a
MbusoMakitla <1699140@students.wits.ac.za>
2019-09-21 21:17:49 +0200

    moved the code to the jobOverview

--- platforms/browser/www/Javascript/jobOverview.js
+++ platforms/browser/www/Javascript/jobOverview.js
@@ -13,6 +13,81 @@ function jobSection() { //look at the transitions
     $("#wrapper_main").load("jobsOverview.html");
 }
 
+function getJobs(){
+    $(".newBrowseJobs").show(function () {
+        //getJobs();
+    });
+    const options = {
+        method: "post",
+        timeout: 10000,
+        data: {
+            ACTION: 1
+        }
+    };
+    const url = "http://1627982.ms.wits.ac.za/~student/Job.php";
+
+    cordova.plugin.http.sendRequest(url, options,
+        function (response) {
+            //success
+            let results = response.data; //data from server, it's a string, must be converted to an appropriate format
+            //e.g. json
+            let jobTable = document.getElementById("jobTable").getElementsByTagName("tbody")[0];
+
+            let output = JSON.parse(results);
+            //console.log(results);
+
+            for(let i=0; i < output.length; i++){
+                //console.log("inside loop");
+                let jobItem = output[i];
+                //console.log(jobItem);
+                let row = jobTable.insertRow();
+                let title = row.insertCell(0);
+                let category = row.insertCell(1);
+                let price_range = row.insertCell(2);
+                let view_more = row.insertCell(3);
+
+
+                title.innerHTML = jobItem["JOB_TITLE"]; //localStorage.getItem("Stud_No")   //localStorage.setItem("Stud_No", userNameInput.val());
+                category.innerHTML = jobItem["JOB_CATEGORY"];
+                price_range.innerHTML = jobItem["JOB_AMOUNT_RANGE_LOW"] + " - " + jobItem["JOB_AMOUNT_RANGE_HIGH"];
+
+
+                let div = document.getElementById("modalBody");
+                let test = document.createTextNode(jobItem["JOB_CATEGORY"]);
+                div.appendChild(test);
+                //Change this
+                view_more.innerHTML = "<a id= \"bidId\" href=\"javascript:void(0);\"><i class=\"material-icons md-dark pmd-sm\" >View more</i></a>";
+                view_more.addEventListener("click", function () {
+                    localStorage.setItem("jobTitle", jobItem["JOB_TITLE"]);
+                    localStorage.setItem("category", jobItem["JOB_CATEGORY"]);
+                    localStorage.setItem("dueDate", jobItem["JOB_DUE_DATE_TIME"]);
+                    localStorage.setItem("NumBids", jobItem["NUM_OF_BIDS"]);
+                    localStorage.setItem("description", jobItem["JOB_DESCRIPTION"]);
+                    localStorage.setItem("job_id", jobItem["JOB_ID"]);
+                    //getBid(); //----------------- Check this out ------------------------
+
+                    $(document).ready(function (){
+                        $("#wrapper_main").load("viewSpecificJobPage.html");
+                    });
+
+                })
+            }
+        },
+        function (response) {
+            //fail
+            let results = response.data;
+            alert("Retrieval of jobs failed");
+            //alert(results);
+
+        },
+        function (response) {
+            //permission denied
+            let results = response.data;
+            alert("You do not have permission to access");
+            //alert(results);
+        });
+}
+
 function myJobs() { //look at the transitions
     $(document).ready(function (){
         $(".newBrowseJobs").hide();
@@ -44,7 +119,7 @@ function myJobs() { //look at the transitions
 
 
                 for(let i=0; i < output.length; i++){
-                    console.log("inside loop");
+                    //console.log("inside loop");
                     let jobItem = output[i];
                     console.log(jobItem);
                     let row = jobTable.insertRow();
--- www/Javascript/bid.js
+++ www/Javascript/bid.js
@@ -2,115 +2,15 @@
 function browseProj() {
     $(document).ready(function () {
 
-        /*$(".jumbotron1").show(function () { //show the bidding page
-            $(".jumbotron1").slideDown(1000);
-        });*/
         $(".myJob").hide();
         $(".offeredJobs1").hide();
         $(".newBrowseJobs").show(function () {
-            getJobs();
+            //getJobs();
         });
-
         //call this after going to the job overview
-        //getBid();
+        getBid();
     });
-    function getJobs(){
-        const options = {
-            method: "post",
-            timeout: 10000,
-            data: {
-                ACTION: 1
-            }
-        };
-        const url = "http://1627982.ms.wits.ac.za/~student/Job.php";
 
-        cordova.plugin.http.sendRequest(url, options,
-            function (response) {
-                //success
-                let results = response.data; //data from server, it's a string, must be converted to an appropriate format
-                //e.g. json
-                let jobTable = document.getElementById("jobTable").getElementsByTagName("tbody")[0];
-
-                let output = JSON.parse(results);
-                //console.log(results);
-
-                for(let i=0; i < output.length; i++){
-                    console.log("inside loop");
-                    let jobItem = output[i];
-                    console.log(jobItem);
-                    let row = jobTable.insertRow();
-                    let title = row.insertCell(0);
-                    let category = row.insertCell(1);
-                    let price_range = row.insertCell(2);
-                    let view_more = row.insertCell(3);
-
-
-                    title.innerHTML = jobItem["JOB_TITLE"]; //localStorage.getItem("Stud_No")   //localStorage.setItem("Stud_No", userNameInput.val());
-                    category.innerHTML = jobItem["JOB_CATEGORY"];
-                    price_range.innerHTML = jobItem["JOB_AMOUNT_RANGE_LOW"] + " - " + jobItem["JOB_AMOUNT_RANGE_HIGH"];
-
-
-                    let div = document.getElementById("modalBody");
-                    let test = document.createTextNode(jobItem["JOB_CATEGORY"]);
-                    div.appendChild(test);
-                    //Change this
-                    view_more.innerHTML = "<a id= \"bidId\" href=\"javascript:void(0);\"><i class=\"material-icons md-dark pmd-sm\" >View more</i></a>";
-                    view_more.addEventListener("click", function () {
-                        localStorage.setItem("jobTitle", jobItem["JOB_TITLE"]);
-                        localStorage.setItem("category", jobItem["JOB_CATEGORY"]);
-                        localStorage.setItem("dueDate", jobItem["JOB_DUE_DATE_TIME"]);
-                        localStorage.setItem("NumBids", jobItem["NUM_OF_BIDS"]);
-                        localStorage.setItem("description", jobItem["JOB_DESCRIPTION"]);
-                        localStorage.setItem("job_id", jobItem["JOB_ID"]);
-                        getBid();
-
-                        $(document).ready(function (){
-                            /*$("#wrapper_main").hide(function () {
-                                $("#wrapper_main").slideUp(1000);
-                            });
-
-                            $("#bidder_page").show(function () {
-                                $("#bidder_page").slideDown(1000);
-                            });*/
-                            $("#wrapper_main").load("viewSpecificJobPage.html");
-                        });
-
-                        /**/
-
-                    })
-                    /*view_more.addEventListener("click", function () { //data-toggle="modal" data-target="#myModal" --this stuff shades the entire page
-                        let modalTitle = document.getElementById("modalTitle");
-                        modalTitle.innerHTML = jobItem["JOB_TITLE"];
-                        let modalPara = document.getElementById("modalBodyText");
-                        modalPara.innerHTML = jobItem["JOB_DESCRIPTION"] + "<br>Job Category: " + jobItem["JOB_CATEGORY"] + "<br> Job payment range: " + jobItem["JOB_AMOUNT_RANGE_LOW"] + " - " + jobItem["JOB_AMOUNT_RANGE_HIGH"];
-
-                        $("#bid_btn1").click(function () {
-                            postBid(jobItem["JOB_ID"]);
-                        });
-                    })*/
-
-                }
-
-
-
-                //alert(results);
-                //alert(results.toString().length);
-                //alert(results.toString()[0]);
-            },
-            function (response) { // we get a respo
-                //fail
-                let results = response.data;
-                alert("1");
-                //alert(results);
-
-            },
-            function (response) {
-                //permission denied
-                let results = response.data;
-                alert("2");
-                //alert(results);
-            });
-    }
     //go home
     $("#home").click(function () { // when you click going to home
         $("#post_job_cont").hide(function () {
@@ -153,22 +53,23 @@ function browseProj() {
                     localStorage.setItem("bidderID", bidItem["BIDDER_ID"]);
                     localStorage.setItem("sug_amt", bidItem["BID_SUGGESTED_AMOUNT"]);
                     localStorage.setItem("bid_msg", bidItem["BID_MESSAGE"]);
+                    //set bid
 
                 }
             },
-            function (response) { // we get a respo
+            function (response) {
                 //fail
-                let results = response.data;
+                //let results = response.data;
                 //alert("2");
-                alert(results);
+                alert("Failed to retrieve bids");
 
             },
             function (response) {
                 //permission denied
                 // alert("3");
-                let results = response.data;
+                //let results = response.data;
                 //alert("2");
-                alert(results);
+                alert("Permission to retrieve bids denied");
             }
         );
     }
@@ -205,17 +106,16 @@ function postBid() {
                 alert("You have bid!");
             }
         },
-        function (response) { // we get a respo
+        function (response) {
             //fail
-            let results = response.data;
-            //alert("2");
-            alert("Bid was posted successfully");
+            //let results = response.data;
+            alert("Bid was posted unsuccessfully");
 
         },
         function (response) {
             //permission denied
-            alert("The bid was unsuccessful");
-            let results = response.data;
+            //alert("The bid was unsuccessful");
+            //let results = response.data;
             //alert("2");
             alert("Permission to post bid was denied");
         }
