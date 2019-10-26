fe82f4f5b6825263bd4a09597432d1d99b24d269
MbusoMakitla <1699140@students.wits.ac.za>
2019-10-26 20:20:27 +0200

    sorting complete

--- platforms/browser/www/Javascript/jobOverview.js
+++ platforms/browser/www/Javascript/jobOverview.js
@@ -35,43 +35,47 @@ function getJobs(){
 
             let output = JSON.parse(results);
             //console.log(results);
+            //default populate
+            populateTableWithInfo(output, jobTable);
 
-            for(let i=0; i < output.length; i++){
-                //console.log("inside loop");
-                let jobItem = output[i];
-                //console.log(jobItem);
-                let row = jobTable.insertRow();
-                let title = row.insertCell(0);
-                let category = row.insertCell(1);
-                let price_range = row.insertCell(2);
-                let view_more = row.insertCell(3);
+            //populate using title
+            $("#titleSort").on("click", () => {
+               output.sort(function (a, b) {
+                   let title1 = a["JOB_TITLE"];
+                   let title2 = b["JOB_TITLE"];
+                   return title1.localeCompare(title2);
+               });
+               populateTableWithInfo(output, jobTable);
+            });
 
+            //populate using category
+            $("#catSort").on("click", () => {
+                console.log("cat pressed");
 
-                title.innerHTML = jobItem["JOB_TITLE"]; //localStorage.getItem("Stud_No")   //localStorage.setItem("Stud_No", userNameInput.val());
-                category.innerHTML = jobItem["JOB_CATEGORY"];
-                price_range.innerHTML = jobItem["JOB_AMOUNT_RANGE_LOW"] + " - " + jobItem["JOB_AMOUNT_RANGE_HIGH"];
+                output.sort(function (a, b) {
+                   let cat1 = a["JOB_CATEGORY"];
+                   let cat2 = b["JOB_CATEGORY"];
+                   return cat1.localeCompare(cat2);
+               });
 
+               populateTableWithInfo(output, jobTable);
+            });
 
-                let div = document.getElementById("modalBody");
-                let test = document.createTextNode(jobItem["JOB_CATEGORY"]);
-                div.appendChild(test);
-                //Change this
-                view_more.innerHTML = "<a id= \"bidId\" href=\"javascript:void(0);\"><i class=\"material-icons md-dark pmd-sm\" >View more</i></a>";
-                view_more.addEventListener("click", function () {
-                    localStorage.setItem("jobTitle", jobItem["JOB_TITLE"]);
-                    localStorage.setItem("category", jobItem["JOB_CATEGORY"]);
-                    localStorage.setItem("dueDate", jobItem["JOB_DUE_DATE_TIME"]);
-                    localStorage.setItem("NumBids", jobItem["NUM_OF_BIDS"]);
-                    localStorage.setItem("description", jobItem["JOB_DESCRIPTION"]);
-                    localStorage.setItem("job_id", jobItem["JOB_ID"]);
-                    //getBid(); //----------------- Check this out ------------------------
+            //populate using price
+            $("#priceSort").on("click", () => {
 
-                    $(document).ready(function (){
-                        $("#wrapper_main").load("viewSpecificJobPage.html");
-                    });
+                console.log("price pressed");
+               output.sort(function (a, b) {
+                   //a["JOB_AMOUNT_RANGE_LOW"];
+                   //return parseInt(a["JOB_AMOUNT_RANGE_LOW"]) - parseInt(b["JOB_AMOUNT_RANGE_LOW"]);
+                   let price1 = parseInt(a["JOB_AMOUNT_RANGE_LOW"]);
+                   let price2 = parseInt(b["JOB_AMOUNT_RANGE_LOW"]);
+                   return price1 - price2;
+               });
 
-                })
-            }
+               populateTableWithInfo(output, jobTable);
+            });
+
         },
         function (response) {
             //fail
@@ -86,6 +90,47 @@ function getJobs(){
             alert("You do not have permission to access");
             //alert(results);
         });
+
+}
+
+function populateTableWithInfo(output, jobTable) {
+    jobTable.innerHTML = "";
+    for(let i=0; i < output.length; i++){
+        //console.log("inside loop");
+        let jobItem = output[i];
+        //console.log(jobItem);
+        let row = jobTable.insertRow();
+        let title = row.insertCell(0);
+        let category = row.insertCell(1);
+        let price_range = row.insertCell(2);
+        let view_more = row.insertCell(3);
+
+
+        title.innerHTML = jobItem["JOB_TITLE"]; //localStorage.getItem("Stud_No")   //localStorage.setItem("Stud_No", userNameInput.val());
+        category.innerHTML = jobItem["JOB_CATEGORY"];
+        price_range.innerHTML = jobItem["JOB_AMOUNT_RANGE_LOW"] + " - " + jobItem["JOB_AMOUNT_RANGE_HIGH"];
+
+
+        let div = document.getElementById("modalBody");
+        let test = document.createTextNode(jobItem["JOB_CATEGORY"]);
+        div.appendChild(test);
+        //Change this
+        view_more.innerHTML = "<a id= \"bidId\" href=\"javascript:void(0);\"><i class=\"material-icons md-dark pmd-sm\" >View more</i></a>";
+        view_more.addEventListener("click", function () {
+            localStorage.setItem("jobTitle", jobItem["JOB_TITLE"]);
+            localStorage.setItem("category", jobItem["JOB_CATEGORY"]);
+            localStorage.setItem("dueDate", jobItem["JOB_DUE_DATE_TIME"]);
+            localStorage.setItem("NumBids", jobItem["NUM_OF_BIDS"]);
+            localStorage.setItem("description", jobItem["JOB_DESCRIPTION"]);
+            localStorage.setItem("job_id", jobItem["JOB_ID"]);
+            //getBid(); //----------------- Check this out ------------------------
+
+            $(document).ready(function (){
+                $("#wrapper_main").load("viewSpecificJobPage.html");
+            });
+
+        })
+    }
 }
 
 function myJobs() { //look at the transitions
@@ -281,9 +326,8 @@ function offers() { //look at the transitions
                         accept_job_btn.innerHTML = "<button type=\"button\" class=\"btn btn-primary\"> Accept Job Offer</button>";
                         complete_btn.innerHTML = "<button type=\"button\" class=\"btn btn-success\">Job Complete</button>";
                         complaintEmployee.innerHTML = "<td id=\"messageBox\" data-title=\"dispute\"><a data-toggle=\"modal\" href=\"\" data-target=\"#myModal\"> Not satisfied? </a></td>";
-                        if(job_status === "2"){
-                            rate.innerHTML = "<td id=\"Rate\" data-title=\"rate\"><a data-toggle=\"modal\" href=\"\" data-target=\"#myModal4\"> Rate </a></td>";
-                        }
+                        //rate.innerHTML = "<td id=\"Rate\" data-title=\"rate\"><a data-toggle=\"modal\" href=\"\" data-target=\"#myModal4\"> Rate </a></td>";
+
                     }
                     complaintEmployee.addEventListener("click", function () {
                         localStorage.setItem("job_id", jobItem["JOB_ID"]);
